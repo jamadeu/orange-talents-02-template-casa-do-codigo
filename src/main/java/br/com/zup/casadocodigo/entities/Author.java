90958a81b603679a564c0d31b5385dece6b57d67
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-11 14:17:38 -0300

    Entity Author

---
+++ src/main/java/br/com/zup/casadocodigo/entities/Author.java
@@ -0,0 +1,58 @@
+package br.com.zup.casadocodigo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    @Email
+    private String email;
+    @NotEmpty
+    @Size(max = 400)
+    private String description;
+    @NotNull
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Author() {
+    }
+
+    public Author(@NotEmpty String name, @NotEmpty @Email String email, @NotEmpty @Size(max = 400) String description) {
+        this.name = name;
+        this.email = email;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
