0d0a61d68687adbfb0879f0a93a57f9336013979
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-12 13:29:39 -0300

    refact: Remove AuthorRepository

--- src/main/java/br/com/zup/casadocodigo/category/controller/CategoryController.java
+++ src/main/java/br/com/zup/casadocodigo/category/controller/CategoryController.java
@@ -2,38 +2,37 @@ package br.com.zup.casadocodigo.category.controller;
 
 import br.com.zup.casadocodigo.category.dto.NewCategoryRequest;
 import br.com.zup.casadocodigo.category.entity.Category;
-import br.com.zup.casadocodigo.category.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+    @PersistenceContext
+    private EntityManager manager;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
-        Optional<Category> optionalCategory = categoryRepository.findById(id);
-        if (optionalCategory.isEmpty()) {
+        Category category = manager.find(Category.class, id);
+        if (category == null) {
             return ResponseEntity.badRequest().body("Category not found");
         }
-        return ResponseEntity.ok(optionalCategory.get());
+        return ResponseEntity.ok(category);
     }
 
     @PostMapping
     @Transactional
     public ResponseEntity<Category> create(@RequestBody @Valid NewCategoryRequest request, UriComponentsBuilder uriBuilder) {
         Category category = request.toModel();
-        categoryRepository.save(category);
+        manager.persist(category);
         URI uri = uriBuilder.path("/category/{id}").buildAndExpand(category.getId()).toUri();
         return ResponseEntity.created(uri).body(category);
     }
