4c1a95c73ea03ed989c75c0a0a98f082b34a24a7
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-12 08:49:14 -0300

    entity book

---
+++ src/main/java/br/com/zup/casadocodigo/book/entity/Book.java
@@ -0,0 +1,99 @@
+package br.com.zup.casadocodigo.book.entity;
+
+import br.com.zup.casadocodigo.author.entity.Author;
+import br.com.zup.casadocodigo.category.entity.Category;
+import br.com.zup.casadocodigo.shared.validator.annotation.FieldUnique;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    @FieldUnique(message = "Title must be unique")
+    private String title;
+    @NotEmpty
+    @Size(max = 500)
+    private String resume;
+    @NotEmpty
+    private String summary;
+    @NotNull
+    @Min(20)
+    private BigDecimal price;
+    @NotNull
+    @Min(100)
+    private Integer numberPages;
+    @NotEmpty
+    @FieldUnique(message = "Title must be unique")
+    private String isbn;
+    @NotNull
+    @Future
+    private LocalDateTime publicationDate;
+    @OneToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    @OneToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @Deprecated
+    public Book() {
+    }
+
+    public Book(@NotEmpty String title, @NotEmpty @Size(max = 500) String resume, @NotEmpty String summary, @NotNull @Min(20) BigDecimal price, @NotNull @Min(100) Integer numberPages, @NotEmpty String isbn, @NotNull @Future LocalDateTime publicationDate, Category category, Author author) {
+        this.title = title;
+        this.resume = resume;
+        this.summary = summary;
+        this.price = price;
+        this.numberPages = numberPages;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+        this.category = category;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getNumberPages() {
+        return numberPages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+}
