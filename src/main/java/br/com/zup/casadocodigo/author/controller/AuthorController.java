17c93aa12b3b1c8fd92d9699dcaa12d1a1ee5bcd
Jean Amadeu <jean.amadeu2@gmail.com>
2021-02-12 13:27:40 -0300

    Remove AuthorRepository

--- src/main/java/br/com/zup/casadocodigo/author/controller/AuthorController.java
+++ src/main/java/br/com/zup/casadocodigo/author/controller/AuthorController.java
@@ -2,38 +2,37 @@ package br.com.zup.casadocodigo.author.controller;
 
 import br.com.zup.casadocodigo.author.dto.NewAuthorRequest;
 import br.com.zup.casadocodigo.author.entity.Author;
-import br.com.zup.casadocodigo.author.repository.AuthorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
 
-    @Autowired
-    private AuthorRepository authorRepository;
+    @PersistenceContext
+    private EntityManager manager;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
-        Optional<Author> optionalAuthor = authorRepository.findById(id);
-        if (optionalAuthor.isEmpty()) {
+        Author author = manager.find(Author.class, id);
+        if (author == null) {
             return ResponseEntity.badRequest().body("Author not found");
         }
-        return ResponseEntity.ok(optionalAuthor.get());
+        return ResponseEntity.ok(author);
     }
 
     @PostMapping
     @Transactional
     public ResponseEntity<Author> create(@RequestBody @Valid NewAuthorRequest request, UriComponentsBuilder uriBuilder) {
         Author author = request.toModel();
-        authorRepository.save(author);
+        manager.persist(author);
         URI uri = uriBuilder.path("/author/{id}").buildAndExpand(author.getId()).toUri();
         return ResponseEntity.created(uri).body(author);
     }
